c30487a352424fdf4a887be3c3b3d11eeb32b618
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-01-04 14:51:08 +0900

    fix

--- _plugins/tags/slideshare.rb
+++ _plugins/tags/slideshare.rb
@@ -6,7 +6,7 @@ module Jekyll
     end
 
     def render(context)
-      %|<iframe src="http://www.slideshare.net/slideshow/embed_code/#{@arg}" width="597" height="486" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #ccc;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen></iframe>|
+      %|<p><iframe src="http://www.slideshare.net/slideshow/embed_code/#{@arg}" width="597" height="486" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #ccc;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen></iframe></p>|
     end
   end
 end
