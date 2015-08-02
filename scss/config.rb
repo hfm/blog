f324d5c5b57acb708e2377f0e103da0eb0bb43cb
Takahiro OKUMURA <hfm.garden@gmail.com>
2015-08-02 09:16:55 +0900

    Move from _sass to scss directory

--- scss/config.rb
+++ scss/config.rb
@@ -1,8 +1,6 @@
 http_path = "/"
 sass_dir = "./"
-css_dir = "../css"
-images_dir = "../img"
-javascripts_dir = "../js"
+css_dir = "../static/css"
 relative_assets = true
 output_style = :compressed
 line_comments = false
